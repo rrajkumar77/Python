182724e27db61bc9578d92933b8cd8357171c969
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2019-05-17 13:02:28 +0530

    Update py_20190407_datatypes_dataprocessing (1).py

--- py_20190407_datatypes_dataprocessing (1).py
+++ py_20190407_datatypes_dataprocessing (1).py
@@ -691,7 +691,7 @@ female students or students with maths score above 60
 
 
 
-## ASSIGNMENT 1
+## ASSIGNMENT 1 - Numpy pandas
 ## DATAFRAME SLICING
 
 # Slice the following
@@ -773,3 +773,57 @@ slice english and gender column of either
 female students or students with maths score above 60
 """
 enggen = df_score.loc[maths60,["English","Gender"]]
+
+
+# Assignment 2 Numpy pandas
+
+# Creating Data frame 
+#Option 1:
+arr = np.random.randint(10,100,100)
+r_arr = np.reshape(arr,[25,4])
+df = pd.DataFrame({'A' : r_arr[:,0],'B':r_arr[:,1],'C' : r_arr[:,2],'D':r_arr[:,3]})
+df.index +=1001
+
+#Option 2:
+df1 = pd.DataFrame(np.random.randint(10,100,size=(25, 4)), columns=list('ABCD'))
+df1.index +=1001
+
+#1. Slice column ‘A’ from df and save it as a series ‘s’
+s =  df.iloc[:,0]
+s1 = df.A
+type(s)
+type(s1)
+
+sliA=df1.A
+type(sliA)
+
+#2. Slice column ‘A’ and column ‘C’ and save it as df2
+
+df2 = df[['A','C']]
+type(df2)
+
+sliac=df1[['A','C']]
+
+#tocheck
+df.iloc["A","C"]
+
+#3. Slice 0th and 2nd column using column number and save it as df3
+df3 = df1.iloc[:,[0,2]]
+
+#4. Slice from 0 till 5th position in series ‘s’
+s[0:5] 
+
+#5. Slice all columns from rows 3 till 19 and save it as df4
+df4 = df1.iloc[3:19]
+
+#6. Create df5 which has subset of data from df where column A values are above median of
+#column A. Note: slice entire columns based on condition on column A
+
+#Option 1
+condA = np.median(df1.A)
+condB = df1.A>condA
+df5 = df1.A[condB]
+
+#Option2
+condC = df1.A > np.median(df1.A)
+df7 = df1.A[condC]
